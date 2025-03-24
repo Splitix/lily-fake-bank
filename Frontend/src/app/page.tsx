c851b5dd10850965b675679719113d59188b477a
Joshua Galindo <splitusa@gmail.com>
2025-03-23 23:33:54 -0500

    updated loading

--- Frontend/src/app/page.tsx
+++ Frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
   const [bank, setBank] = useState({
-    amount: "",
+    amount: "...",
   });
 
   useEffect(() => {
