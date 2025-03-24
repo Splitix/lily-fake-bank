e5290004ae3767d5d2a97cbd313cff77b811b467
Joshua Galindo <splitusa@gmail.com>
2025-03-24 00:36:52 -0500

    Fixed Start up script

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-screen -d -m bash -c "cd Frontend/ && npm run dev"
 screen -d -m bash -c "cd Backend/ && npm start"
+screen -d -m bash -c "cd Frontend/ && npm run dev"
+sleep  15
+screen -d -m bash -c "DISPLAY=:0.0 firefox -kiosk -url http://localhost:3000"
